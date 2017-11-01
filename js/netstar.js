c84f8f40ac9a6287a080412dbf5a0ac7bc2e74ca
xmoyking <xmoyking@gmail.com>
2017-11-01 19:23:53 +0800

    主播榜单17.11.1更新

---
+++ js/netstar.js
@@ -0,0 +1,50 @@
+var ia =   [
+    [["排行","姓名","标签","类型","粉丝数","平台","房间连接","头像"],[1,"White55开解说","卢本伟 牛逼 表面兄弟 凉凉","英雄联盟","12739520","斗鱼","http://www.douyu.com/138286","https://apic.douyucdn.cn/upload/avanew/face/201612/30/02/c14ddc0a4d121d32fc5d150f0e97448b_big.jpg?rltime?rltime"],[2,"冯提莫","好听 一米五 人美歌甜 活泼可爱","英雄联盟","10392150","斗鱼","http://www.douyu.com/71017","https://apic.douyucdn.cn/upload/avanew/face/201710/13/19/cbfc90cd87d88c263776a5f1c528480f_big.jpg?rltime?rltime"],[3,"芜湖大司马丶","真皮沙发 皮皮 弱智 老师","英雄联盟","9128445","斗鱼","http://www.douyu.com/606118","https://apic.douyucdn.cn/upload/avatar/face/201609/08/f6c0c08a3f3cb1fd074fe38907ae2508_big.jpg?rltime?rltime"],[4,"陈一发儿","一发 关注 超级 话题","主机游戏","7634846","斗鱼","http://www.douyu.com/67373","https://apic.douyucdn.cn/upload/avatar/002/28/65/01_avatar_big.jpg?rltime?rltime"],[5,"老实敦厚的笑笑","秃秃秃秃 老油子 办卡 德云色","英雄联盟","6031669","斗鱼","http://www.douyu.com/154537","https://apic.douyucdn.cn/upload/avanew/face/201706/29/20/64b39bcf6c3e3e46972258a26d5b78df_big.jpg?rltime?rltime"],[6,"即将拥有人鱼线的PDD","末日 人机 老师 渔夫","英雄联盟","5845141","熊猫","https://www.panda.tv/6666","http://i6.pdim.gs/fe6c1edaa50f1ae3f346f0452a9254ed.jpeg"],[7,"指法芬芳张大仙","大仙 没人 哈哈哈 学校","王者荣耀","5578728","斗鱼","http://www.douyu.com/688","https://apic.douyucdn.cn/upload/avanew/face/201708/03/12/8768fecb8233437a579dc932d7f0a21c_big.jpg?rltime?rltime"],[8,"英雄联盟官方赛事","关注 不亏 lpl LPL","英雄联盟","4116078","斗鱼","http://www.douyu.com/288016","https://apic.douyucdn.cn/upload/avanew/face/201709/05/20/1f3ce55829bc50da263a938bd81aebfc_big.jpg"],[9,"主播油条","喷子 15 油条 科技","绝地求生","4112368","斗鱼","http://www.douyu.com/56040","https://apic.douyucdn.cn/upload/avatar/001/00/17/26_avatar_big.jpg?rltime?rltime"],[10,"Misaya若风lol","亚索 风队 男人 miss","绝地求生","3958533","熊猫","https://www.panda.tv/666666","http://i1.pdim.gs/t019b937b40d8b95c41.jpg"],[11,"七哥张琪格","emot 牌子 多人 换个","炉石传说","3708507","斗鱼","http://www.douyu.com/65251","https://apic.douyucdn.cn/upload/avanew/face/201703/15/02/8f8792bfd7e8ccc29ce69ea35298f7d8_big.jpg?rltime?rltime"],[12,"东北大鹌鹑","生日快乐 鹌鹑 山驴 生日","英雄联盟","3618550","斗鱼","http://www.douyu.com/96291","https://apic.douyucdn.cn/upload/avanew/face/201610/17/19/b1060e87c9d17bd37e22c6a63818f07e_big.jpg?rltime?rltime"],[13,"韦神","节奏 谢谢 今天 大气","绝地求生","3592590","斗鱼","http://www.douyu.com/7911","https://apic.douyucdn.cn/upload/avatar/face/201606/27/11d6864e4e6437c95af780abe1862e30_big.jpg?rltime?rltime"],[14,"洞主丨歌神洞庭湖","真皮沙发 弱智 真皮 主播","英雄联盟","3013718","斗鱼","http://www.douyu.com/138243","https://apic.douyucdn.cn/upload/avanew/face/201708/23/22/7de486706f57f007704560894ea4f7d5_big.jpg?rltime?rltime"],[15,"饼干狂魔MasterB","i7 才会赢 饼干 大家","英雄联盟","2996252","斗鱼","http://www.douyu.com/4809","https://apic.douyucdn.cn/upload/avatar/000/18/41/65_avatar_big.jpg?rltime?rltime"],[16,"阿冷aleng丶","一天 冷冷 愿得 200","英雄联盟","2954324","斗鱼","http://www.douyu.com/2371789","https://apic.douyucdn.cn/upload/avanew/face/201710/27/15/30bfcb5821ced2bb6898fedc8ca8f38e_big.jpg?rltime?rltime"],[17,"周二珂","今天 今晚 开播 5000","绝地求生","2853834","熊猫","https://www.panda.tv/35723","http://i7.pdim.gs/dmfd/200_200_100/0257040380bd7d294a8017532c07e39b.jpg"],[18,"guoyun丶mini","古风女神 郭老师 送送送 抽奖","炉石传说","2651397","斗鱼","http://www.douyu.com/10903","https://apic.douyucdn.cn/upload/avanew/face/201710/24/02/ebbc31c0828e2573aa114e7be0be7f43_big.jpg?rltime?rltime"],[19,"我叫撸管飞","i7 韩服 主播 白银","英雄联盟","2601887","斗鱼","http://www.douyu.com/453751","https://apic.douyucdn.cn/upload/avanew/face/201710/20/23/aa26204b7967594b01fa5d4c1c8bb8ee_big.jpg"],[20,"嗨氏","疯狂 call 海涛 哥哥","王者荣耀","2577457","斗鱼","http://www.douyu.com/1229","https://apic.douyucdn.cn/upload/avanew/face/201708/26/21/15f974b260c643688cd5b83480d19ccf_big.jpg?rltime?rltime"]]
+    ]
+
+var title = $('#sec-title');
+var chart = $('#chart-data');
+// 替换标题和时间
+title.html('<span>主播</span>排行榜TOP10').parent().append('<p> 更新时间: 2017/11/01 </p>');
+// 替换tab
+var tab = '<div class="charts-kinds">\
+            <a href="javascript:;"class="j-tab selected">粉丝数</a>\
+           </div>';
+chart.html(tab);
+
+Handlebars.registerHelper('selected', function(idx, opt){
+    if(idx === 0) return 'selected';
+});
+
+Handlebars.registerHelper('rank', function(idx, opt){
+    if(idx === '排行') return idx;
+    if(idx <= 3) return '<i class="rank-T3">'+idx+'</i>';
+    return '<i class="rank-other">'+idx+'</i>';
+});
+
+var olstr ='<style> .chart-list li:first-child img{display:none;} img{width:20px;}.t-2{width:200px}.t-3{width:250px}.chart-list:nth-of-type(4).t-5{width:100px}.t-6{width:120px}@media(max-width:767px){.chart-list.t-4{display:inline-block}}</style>\
+{{#each this}}<ol class="chart-list j-for {{#selected @index}}{{/selected}}">\
+    {{#with this}}{{#each this}}<li>\
+        <span class="t-1">{{#rank this.[0]}}{{/rank}}</span>\
+        <span class="t-2"><a href="{{this.[6]}} "title="点击查看：{{this.[1]}} "target="_blank"><img src="{{this.[7]}}"> {{this.[1]}}</a></span>\
+        <span class="t-3">{{this.[2]}}</span>\
+        <span class="t-4">{{this.[3]}}</span>\
+        <span class="t-5 text-center">{{this.[4]}}</span>\
+        <span class="t-1 text-center">{{this.[5]}}</span>\
+    </li>{{/each}}\
+{{/with}}</ol>{{/each}}';
+
+
+
+var template = Handlebars.compile(olstr);
+var allstr = template(ia);
+chart.append(allstr);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$('.j-tab').hover(function() {
+    if ($(this).hasClass('selected')) {
+        return false;
+    }
+    $(this).siblings().removeClass('selected').end().addClass('selected');
+    $('.j-for').removeClass('selected').eq($(this).index()).addClass('selected');
+});
